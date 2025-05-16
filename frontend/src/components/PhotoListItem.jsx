1b2be22e18022c818c4db8cc34413f5ae5b0e18e
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-15 21:26:20 -0500

    added styling to photolist items

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,17 +4,18 @@ const PhotoListItem = ({ photo }) => {
   /* Insert React */
   const { id, location, urls, user } = photo;
   return(
-      <div className="photo-list-item" key={id}>
-      <img src={urls.regular} alt="Regular size image" className="photo-regular" />
-      <div className="photographer-info">
-        <img
-          src={user.profile}
-          alt={`${user.username}'s profile`}
-          className="profile-pic"
-        />
-        <div className="username">{user.name}</div>
+      <div className="photo-list__item" key={id}>
+      <img src={urls.regular} alt="Regular size image" className="photo-list__image" />
+      
+      <div className="photo-list__user-details">
+        <img src={user.profile} alt={`${user.username}'s profile`} className="photo-list__user-profile"/>
+        
+        <div className="photo-list__user-info">{user.name}
+          <div className="photo-list__user-location">{`${location.city}, ${location.country}`}</div>
+        </div>
       </div>
-      <div className="location">{`${location.city}, ${location.country}`}</div>
+      
+      
     </div>
   )
 
