83168f4ddf0a44bece16331561295d0d2a775fc6
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-18 14:03:22 -0500

    refactored with mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,28 +1,11 @@
+import topics from "../mocks/topics";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: 1,
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: 2,
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: 3,
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = () => {
   return (
     <nav className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topic) => (
+      {topics.map((topic) => (
         <span key={topic.id}>
           <TopicListItem topic={topic}/>
         </span>
