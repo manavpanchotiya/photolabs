28197115dbdba6132deb21e48e537d5b8c798f78
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-18 13:41:10 -0500

    added topNavigationBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 //import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
+// import TopicList from './components/TopicList';
+import TopNavigation from './components/TopNavigationBar';
 
 const App = () => {
   return (
+    <>
     <div className="App">
-      <TopicList/>      
+      <TopNavigation/>
       <PhotoList/>
     </div>
+    </>
   );
 };
 export default App;
