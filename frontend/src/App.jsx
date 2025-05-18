12015ddbdefbc48fdb1f677950024e358819435e
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-18 13:49:32 -0500

    added HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 //import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import PhotoList from './components/PhotoList';
-// import TopicList from './components/TopicList';
-import TopNavigation from './components/TopNavigationBar';
+import HomeRoute from './components/HomeRoute';
 
 const App = () => {
   return (
     <>
     <div className="App">
-      <TopNavigation/>
-      <PhotoList/>
+      <HomeRoute/>
     </div>
     </>
   );
---
+++ frontend/src/components/HomeRoute.jsx
@@ -0,0 +1,12 @@
+import PhotoList from "./PhotoList"
+import TopNavigation from "./TopNavigationBar"
+
+const HomeRoute = () => {
+    return (
+        <div className="home-route">
+            <TopNavigation/>
+            <PhotoList/>
+        </div>
+    );
+};
+export default HomeRoute;
